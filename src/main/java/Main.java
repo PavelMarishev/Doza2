2880abbf163e6214c1490e5f356f26e661414812
PavelMarishev <karthusss@ya.ru>
2016-11-29 23:38:01 +0200

    Fixed XPath Error + added cleaner+ added init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         WebDriver wd =new FirefoxDriver();
         wd.get("http://80.92.229.236:81/auth/login");
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         WebElement usernameInput= wd.findElement(By.id("username"));
         usernameInput.sendKeys("admin");
         WebElement passInput= wd.findElement(By.id("password"));
@@ -25,18 +25,15 @@ public class Main {
         if(actualtitle.equals(expectedtitle)) System.out.println("PASSED");
         else
             System.err.println("Failed. Expected title is "+expectedtitle+" and Actual is"+actualtitle);
-
-        wd.findElement(By.xpath(".//*[@title=\"Insert\"]")).click();
-
+          wd.findElement(By.xpath(".//*[@title=\"Insert\"]")).click();
        Tester tester =new Tester(wd);
-
-       tester.Writer();
-       tester.OpenChanger();
+           tester.Writer();
+      tester.OpenChanger();
        tester.Checker();
        tester.Writer();
        tester.OpenChanger();
        tester.Checker();
-        wd.quit();
+       wd.quit();
 
 
     }
